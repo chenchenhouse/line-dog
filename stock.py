034e61a82f8836de187f6148945351a9c8ca118c
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-02 18:38:35 +0800

    Add files via upload

--- stock.py
+++ stock.py
@@ -3,20 +3,20 @@ from bs4 import BeautifulSoup
 
 
 def stock_id(message):
-    url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text,"html.parser")
-    soup1 = soup.find("table",{"class":"h4"})
-    s_id = []
-    s_name = []
-    for i in range(len(soup1.find_all("tr")[1:])):
-        s_id_ = soup1.find_all("tr")[i+1].text.split("\n")[3]
-        s_id.append(s_id_)
-        s_name_ = soup1.find_all("tr")[i+1].text.split("\n")[4]
-        s_name.append(s_name_)
-    found_index = s_name.index(message)
-    f_id = s_id[found_index]
-    url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(f_id)
+    # url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
+    # res = requests.get(url)
+    # soup = BeautifulSoup(res.text,"html.parser")
+    # soup1 = soup.find("table",{"class":"h4"})
+    # s_id = []
+    # s_name = []
+    # for i in range(len(soup1.find_all("tr")[1:])):
+    #     s_id_ = soup1.find_all("tr")[i+1].text.split("\n")[3]
+    #     s_id.append(s_id_)
+    #     s_name_ = soup1.find_all("tr")[i+1].text.split("\n")[4]
+    #     s_name.append(s_name_)
+    # found_index = s_name.index(message)
+    # f_id = s_id[found_index]
+    url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
     }
