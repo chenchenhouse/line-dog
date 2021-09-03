2eece46c5dbb737579d46646cc758789ae9eceb0
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-03 21:56:33 +0800

    Add files via upload

--- stock_title.py
+++ stock_title.py
@@ -1,10 +1,11 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials as SAC
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
+
 
 def stock_name():
+    import gspread
+    from oauth2client.service_account import ServiceAccountCredentials as SAC
+    import requests
+    from bs4 import BeautifulSoup
+    from datetime import datetime
     Json = 'stock-search-324803-9c7ec6c7c26c.json' # Json 的單引號內容請改成妳剛剛下載的那個金鑰
     Url = ['https://spreadsheets.google.com/feeds']
     Connect = SAC.from_json_keyfile_name(Json, Url)
@@ -19,21 +20,23 @@ def stock_name():
     res2 = requests.get(url2)
     soup2 = BeautifulSoup(res2.text,"html.parser")
     soup3 = soup2.find("table",{"class":"h4"})
-    # s_id = []
-    # for i in range(len(soup1.find_all("tr"))):
-    #     s_id_ = soup1.find_all("tr")[i].text.split("\n")[3:5]
-    #     s_id.append(s_id_)
-    # for i in range(len(soup3.find_all("tr"))):
-    #     s_id_ = soup3.find_all("tr")[i].text.split("\n")[3:5]
-    #     s_id.append(s_id_)
-    # if len(Sheets.get_all_records()) == 0:
-    #     Sheets.append_rows(s_id)
-    # else:
-    #     Sheets.update(s_id)
+    s_id = []
+    for i in range(len(soup1.find_all("tr"))):
+        s_id_ = soup1.find_all("tr")[i].text.split("\n")[3:5]
+        s_id.append(s_id_)
+    for i in range(len(soup3.find_all("tr"))):
+        s_id_ = soup3.find_all("tr")[i].text.split("\n")[3:5]
+        s_id.append(s_id_)
+    if len(Sheets.get_all_records()) == 0:
+        Sheets.append_rows(s_id)
+    else:
+        Sheets.update(s_id)
     return "{} 更新完成".format(datetime.now().strftime('%Y-%m-%D %H:%M:%S'))
 
 
 def found_id(stock):
+    import gspread
+    from oauth2client.service_account import ServiceAccountCredentials as SAC
     Json = 'stock-search-324803-9c7ec6c7c26c.json' # Json 的單引號內容請改成妳剛剛下載的那個金鑰
     Url = ['https://spreadsheets.google.com/feeds']
     Connect = SAC.from_json_keyfile_name(Json, Url)
