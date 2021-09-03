52769265cf06bf12eef14a28df0e304c0537a322
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-03 21:32:32 +0800

    Add files via upload

--- stock_title.py
+++ stock_title.py
@@ -19,23 +19,20 @@ def stock_name():
     res2 = requests.get(url2)
     soup2 = BeautifulSoup(res2.text,"html.parser")
     soup3 = soup2.find("table",{"class":"h4"})
-    new_func(Sheets, soup1, soup3)
+    # s_id = []
+    # for i in range(len(soup1.find_all("tr"))):
+    #     s_id_ = soup1.find_all("tr")[i].text.split("\n")[3:5]
+    #     s_id.append(s_id_)
+    # for i in range(len(soup3.find_all("tr"))):
+    #     s_id_ = soup3.find_all("tr")[i].text.split("\n")[3:5]
+    #     s_id.append(s_id_)
+    # if len(Sheets.get_all_records()) == 0:
+    #     Sheets.append_rows(s_id)
+    # else:
+    #     Sheets.update(s_id)
     return "{} 更新完成".format(datetime.now().strftime('%Y-%m-%D %H:%M:%S'))
 
-def new_func(Sheets, soup1, soup3):
-    s_id = []
-    for i in range(len(soup1.find_all("tr"))):
-        s_id_ = soup1.find_all("tr")[i].text.split("\n")[3:5]
-        s_id.append(s_id_)
-    for i in range(len(soup3.find_all("tr"))):
-        s_id_ = soup3.find_all("tr")[i].text.split("\n")[3:5]
-        s_id.append(s_id_)
-    if len(Sheets.get_all_records()) == 0:
-        Sheets.append_rows(s_id)
-    else:
-        Sheets.update(s_id)
 
-    
 def found_id(stock):
     Json = 'stock-search-324803-9c7ec6c7c26c.json' # Json 的單引號內容請改成妳剛剛下載的那個金鑰
     Url = ['https://spreadsheets.google.com/feeds']
