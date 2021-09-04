fbee934e0cedcbcd83c43d099ce4d40f172bb1ed
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-04 13:59:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import re
 #*********function*****************
 from blog import *
 from stock import *
-from stockname import *
 #*********function*****************
 
 
@@ -57,11 +56,7 @@ def handle_message(event):
         # Flex Message Simulator網頁：https://developers.line.biz/console/fx/
       flex_message = flex()
       line_bot_api.reply_message(event.reply_token,flex_message)
-    elif re.match('更新',message):
-        update = stock_name()
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(update))
     elif "股票 " in message:
-        #found = found_id(message[3:])
         stock_message = stock_id(message[3:])
         line_bot_api.reply_message(event.reply_token,TextSendMessage(stock_message))
     else:
--- stock.py
+++ stock.py
@@ -1,9 +1,24 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup 
-
+import re
 
 
 def stock_id(message):
+    if not re.match(r'[+-]?\d+$', message):
+        url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
+        df = pd.read_html(requests.get(url).text)[0]
+        df = df.iloc[:,2:7]
+        df.columns = df.iloc[0,:]
+        df = df[1:]
+        url2 = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y"
+        df2 = pd.read_html(requests.get(url2).text)[0]
+        df2 = df2.iloc[:,2:7]
+        df2.columns = df2.iloc[0,:]
+        df2 = df2[1:]
+        df3 = pd.concat([df,df2])
+        df4 = df3[df3["有價證券名稱"] == message]
+        message = df4.values[0,0]
     url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
@@ -17,5 +32,5 @@ def stock_id(message):
     soup4 = soup3.find("a").text.split("\xa0")
     mes = "股票代號 :{} \n股票名稱 : {} \n成交價 : {} \n昨收 : {} \n漲跌價 : {} \n漲跌幅 : {} \n振幅 : {} \n開盤 : {} \n最高 : {} \n最低 : {}".format(soup4[0],soup4[1],soup2[0],soup2[1],soup2[2],soup2[3],soup2[4],soup2[5],soup2[6],soup2[7])
     return mes
-
+ 
 
