c6dacf531a5153064c179dd0d98c629962c89ad9
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-04 14:08:08 +0800

    Add files via upload

--- stock.py
+++ stock.py
@@ -19,18 +19,20 @@ def stock_id(message):
         df3 = pd.concat([df,df2])
         df4 = df3[df3["有價證券名稱"] == message]
         message = df4.values[0,0]
-    url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
-    }
-    res = requests.get(url,headers = headers)
-    res.encoding = "utf-8"
-    soup = BeautifulSoup(res.text,"html.parser")
-    soup1 = soup.find("table",{"class":"b1 p4_2 r10"})
-    soup2 = soup1.find("tr",{"align":"center"}).text.split(" ")[1:-1]
-    soup3 = soup.find("td",{"style":"padding:0 2px 5px 20px;width:10px;"})
-    soup4 = soup3.find("a").text.split("\xa0")
-    mes = "股票代號 :{} \n股票名稱 : {} \n成交價 : {} \n昨收 : {} \n漲跌價 : {} \n漲跌幅 : {} \n振幅 : {} \n開盤 : {} \n最高 : {} \n最低 : {}".format(soup4[0],soup4[1],soup2[0],soup2[1],soup2[2],soup2[3],soup2[4],soup2[5],soup2[6],soup2[7])
-    return mes
- 
+        return("123")
+    else:
+        url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+        }
+        res = requests.get(url,headers = headers)
+        res.encoding = "utf-8"
+        soup = BeautifulSoup(res.text,"html.parser")
+        soup1 = soup.find("table",{"class":"b1 p4_2 r10"})
+        soup2 = soup1.find("tr",{"align":"center"}).text.split(" ")[1:-1]
+        soup3 = soup.find("td",{"style":"padding:0 2px 5px 20px;width:10px;"})
+        soup4 = soup3.find("a").text.split("\xa0")
+        mes = "股票代號 :{} \n股票名稱 : {} \n成交價 : {} \n昨收 : {} \n漲跌價 : {} \n漲跌幅 : {} \n振幅 : {} \n開盤 : {} \n最高 : {} \n最低 : {}".format(soup4[0],soup4[1],soup2[0],soup2[1],soup2[2],soup2[3],soup2[4],soup2[5],soup2[6],soup2[7])
+        return mes
+    
 
