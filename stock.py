35bdc0d848a8149cef971724e4160b7a104cc1c6
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-04 14:16:12 +0800

    Add files via upload

--- stock.py
+++ stock.py
@@ -6,19 +6,19 @@ import re
 
 def stock_id(message):
     if not re.match(r'[+-]?\d+$', message):
-        # url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
-        # df = pd.read_html(requests.get(url).text)[0]
-        # df = df.iloc[:,2:7]
-        # df.columns = df.iloc[0,:]
-        # df = df[1:]
-        # url2 = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y"
-        # df2 = pd.read_html(requests.get(url2).text)[0]
-        # df2 = df2.iloc[:,2:7]
-        # df2.columns = df2.iloc[0,:]
-        # df2 = df2[1:]
-        # df3 = pd.concat([df,df2])
-        # df4 = df3[df3["有價證券名稱"] == message]
-        # message = df4.values[0,0]
+        url = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
+        df = pd.read_html(requests.get(url).text)[0]
+        df = df.iloc[:,2:7]
+        df.columns = df.iloc[0,:]
+        df = df[1:]
+        url2 = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y"
+        df2 = pd.read_html(requests.get(url2).text)[0]
+        df2 = df2.iloc[:,2:7]
+        df2.columns = df2.iloc[0,:]
+        df2 = df2[1:]
+        df3 = pd.concat([df,df2])
+        df4 = df3[df3["有價證券名稱"] == message]
+        message = df4.values[0,0]
         return("123")
     else:
         url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
