e0575b3b61d479a6455eea5de6e15152be9228e5
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-09-06 13:06:44 +0800

    Add files via upload

--- stock.py
+++ stock.py
@@ -49,24 +49,21 @@ def stock_id(message):
 def compare_one(message):
     if not re.match(r'[+-]?\d+$', message):
         message = stock_change(message)
-    try:
-        url = "https://tw.stock.yahoo.com/quote/" +str(message)+"/compare"
-        headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
-        }
+    url = "https://tw.stock.yahoo.com/quote/" +str(message)+"/compare"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    }
+    res = requests.get(url,headers= headers)
+    res.encoding = "utf-8"
+    soup = BeautifulSoup(res.text,"html.parser")
+    while soup.text =="\n\n\n\n\n":
         res = requests.get(url,headers= headers)
         res.encoding = "utf-8"
         soup = BeautifulSoup(res.text,"html.parser")
-        while soup.text =="\n\n\n\n\n":
-            res = requests.get(url,headers= headers)
-            res.encoding = "utf-8"
-            soup = BeautifulSoup(res.text,"html.parser")
-        soup1 = soup.find("a",{"class":"D(ib) Fz(14px) Lh(20px) C($c-button) Mb(20px) Mb(16px)--mobile C($c-active-text):h Td(n)"}).text
-        soup2 = soup.find_all("span",{"class":"C(#000) Fz(24px) Fw(600)"})
-        message = "{} \n近一年漲跌幅 : 第{}名 \n近一年每股盈餘 : 第{}名 \n近一年殖利率 : 第{}名".format(soup1,soup2[0].text,soup2[1].text,soup2[2].text)
-        return message
-    except:
-        return("請輸入正確的股票代號")
+    soup1 = soup.find("a",{"class":"D(ib) Fz(14px) Lh(20px) C($c-button) Mb(20px) Mb(16px)--mobile C($c-active-text):h Td(n)"}).text
+    soup2 = soup.find_all("span",{"class":"C(#000) Fz(24px) Fw(600)"})
+    message = "{} \n近一年漲跌幅 : 第{}名 \n近一年每股盈餘 : 第{}名 \n近一年殖利率 : 第{}名".format(soup1,soup2[0].text,soup2[1].text,soup2[2].text)
+    return message
 
 def compare_other(message):    
     if not re.match(r'[+-]?\d+$', message):
@@ -86,7 +83,7 @@ def compare_other(message):
     stock_id_ = soup.find_all("span",{"class":"Fz(14px) C(#979ba7) Ell"})
     stock_name = soup.find_all("div",{"class":"Lh(20px) Fw(600) Fz(16px) Ell"})
     stock_quote = soup.find_all("div",{"class":"Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(100px) Bgc(t)"})
-    for i in range(len(stock_id)):
+    for i in range(len(message)):
         compare += "{}\t{}\t\t{} \n".format(stock_id_[i].text,stock_name[i].text,stock_quote[i].text)
     return(compare)
 
