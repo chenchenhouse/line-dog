b370beb9b1fa4dcc8c7b3dc529de86b00dba108c
chenchenhouse <74691006+chenchenhouse@users.noreply.github.com>
2021-10-05 12:07:54 +0800

    Add files via upload

--- stock_list.py
+++ stock_list.py
@@ -3,6 +3,8 @@ import pandas as pd
 import requests
 from stock import  stock_change
 import re
+from bs4 import BeautifulSoup
+import emoji
 
 #新增股票到關注清單
 def stock_database_add(message):
@@ -82,7 +84,7 @@ def stock_database_del(message):
         else:
             return stock_i + stock_n + " 並非已關注股票"
     except:
-        return "查無您輸入的" + message + "，請重新輸入確認"
+        return "查無您輸入的" + message + "，請重新輸入"
 
 #查詢清單
 def find_list():
@@ -96,7 +98,21 @@ def find_list():
     cursor.execute("SELECT * FROM `list`;")
     records = cursor.fetchall()
     s_d = pd.DataFrame(records,columns=["股票代號","股票名稱"])
-    list_n = "目前關注中的股票有 : \n"
+    context = ""
     for i in range(len(s_d)):
-        list_n += s_d.iloc[i][0] + " " + s_d.iloc[i][1] + "\n"
-    return list_n
+        message = s_d.iloc[i][0]
+        url = "https://goodinfo.tw/StockInfo/StockDetail.asp?STOCK_ID=" + str(message)
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+        }
+        res = requests.get(url,headers = headers)
+        res.encoding = "utf-8"
+        soup = BeautifulSoup(res.text,"html.parser")
+        soup1 = soup.find("table",{"class":"b1 p4_2 r10"})
+        soup2 = soup1.find("tr",{"align":"center"}).text.split(" ")[1:-1]
+        soup3 = soup.find("td",{"style":"padding:0 2px 5px 20px;width:10px;"})
+        soup4 = soup3.find("a").text.split("\xa0")
+        soup_1 = soup.find("td",{"style":"padding:0 18px 5px 0;text-align:right;"})
+        star = emoji.emojize(':small_blue_diamond:')
+        context += "{} {} 最新資訊 \n-------------------------- \n{} {}\n{} 最新成交價 : {} \n{} 開盤價 : {} \n{} 最高價 : {} \n{} 最低價 : {} \n{} 漲跌幅 : {} \n--------------------------\n".format(soup4[0],soup4[1],emoji.emojize(':clock9:',use_aliases=True),soup_1.text,star,soup2[0],star,soup2[5],star,soup2[6],star,soup2[7],star,soup2[3])
+    return context
